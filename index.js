3b135e29cd3fa997f18113ea4c74b143aa34ad81
Christian <your_email@example.com>
2026-03-29 13:02:22 +0300

    feat: update on objects lab-js

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 //Write your code here
+// Create an object named `attendee` with the specified properties
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+// Function to log the attendee's name
+function logAttendeeName(attendee) {  
+  console.log(attendee.name);
+}
 
+// Function to log the ticket price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+// Function to update the ticket type
+function updateTicketType(attendee, newType) {
+  attendee.ticketType = newType;
+}
+
+// Function to update the ticket price
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+// Function to remove the event property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Function to add a checkedIn property with a value of true
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
+
+// Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
